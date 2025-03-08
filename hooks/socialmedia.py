10d9e0deefddb7b7f9efecf5c6a9ad585cfdb136
Matt Dies <matthewkdies@gmail.com>
2025-03-08 23:17:09 +0000

    Customizing social media links.

--- hooks/socialmedia.py
+++ hooks/socialmedia.py
@@ -1,21 +1,21 @@
-from textwrap import dedent
-import urllib.parse
 import re
+import urllib.parse
+from textwrap import dedent
 
-x_intent = "https://twitter.com/intent/tweet"
-fb_sharer = "https://www.facebook.com/sharer/sharer.php"
+from mkdocs.config.defaults import MkDocsConfig
+from mkdocs.structure.files import Files
+from mkdocs.structure.pages import Page
+
+# I'm not using "X" -- what am I, a racist?
+bluesky_intent = "https://bsky.app/intent/compose"
 include = re.compile(r"blog/[1-9].*")
 
-def on_page_markdown(markdown, **kwargs):
-    page = kwargs['page']
-    config = kwargs['config']
+
+def on_page_markdown(markdown: str, page: Page, config: MkDocsConfig, files: Files):
     if not include.match(page.url):
         return markdown
 
-    page_url = config.site_url+page.url
-    page_title = urllib.parse.quote(page.title+'\n')
+    page_url = config.site_url + page.url
+    text = urllib.parse.quote(f"{page.title}\n{page_url}")
 
-    return markdown + dedent(f"""
-    [Share on :simple-x:]({x_intent}?text={page_title}&url={page_url}){{ .md-button }}
-    [Share on :simple-facebook:]({fb_sharer}?u={page_url}){{ .md-button }}
-    """)
+    return markdown + dedent(f"\n\n[Share on :simple-bluesky:]({bluesky_intent}?text={text}){{ .md-button }}")
